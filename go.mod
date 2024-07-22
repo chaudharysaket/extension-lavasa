9721b4283658c1eae087a6f8bbb20210b01e721a
Saket Chaudhary <saketchaudharynit@gmail.com>
2024-07-22 20:06:18 +0530

    add logs

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module extension-lavasa
 
 go 1.21.4
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/golang-collections/go-datastructures v0.0.0-20150211160725-59788d5eb259
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -10,22 +10,36 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strings"
 	"syscall"
 
 	"extension-lavasa/extension"
+	"extension-lavasa/logsapi"
+	agent "extension-lavasa/agent"
+
+	"github.com/golang-collections/go-datastructures/queue"
+
+	log "github.com/sirupsen/logrus"
 )
+
+// INITIAL_QUEUE_SIZE is the initial size set for the synchronous logQueue
+const INITIAL_QUEUE_SIZE = 5
 
 var (
 	extensionName   = filepath.Base(os.Args[0]) // extension name has to match the filename
-	extensionClient = extension.NewClient(os.Getenv("AWS_LAMBDA_RUNTIME_API"))
 	printPrefix     = fmt.Sprintf("[%s]", extensionName)
+	extensionClient = extension.NewClient(os.Getenv("AWS_LAMBDA_RUNTIME_API"))
 )
+
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+	logger := log.WithFields(log.Fields{"agent": extensionName})
+
+
 	go func() {
 		s := <-sigs
 		cancel()
@@ -37,35 +51,64 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	logQueue := queue.New(INITIAL_QUEUE_SIZE)
+	var logsStr string = ""
+	flushLogQueue := func(force bool) {
+	for !(logQueue.Empty() && (force || strings.Contains(logsStr, string(logsapi.RuntimeDone)))) {
+		logs, err := logQueue.Get(1)
+		if err != nil {
+			logger.Error(printPrefix, err)
+			return
+		}
+		logsStr = fmt.Sprintf("%v", logs[0])
+		if err != nil {
+			logger.Error(printPrefix, err)
+			return
+		}
+	}
+	}
 	println(printPrefix, "Register response:", prettyPrint(res))
+	// Create Logs API agent
+	logsApiAgent, err := agent.NewHttpAgent(logQueue)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	// Subscribe to logs API
+	// Logs start being delivered only after the subscription happens.
+	agentID := extensionClient.ExtensionID
+	err = logsApiAgent.Init(agentID)
+	if err != nil {
+		logger.Fatal(err)
+	}
 
 	// Will block until shutdown event is received or cancelled via the context.
-	processEvents(ctx)
-}
-
-func processEvents(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		default:
-			println(printPrefix, "Waiting for event...")
+			logger.Info(printPrefix, " Waiting for event...")
+			// This is a blocking call
 			res, err := extensionClient.NextEvent(ctx)
 			if err != nil {
-				println(printPrefix, "Error:", err)
-				println(printPrefix, "Exiting")
+				logger.Info(printPrefix, "Error:", err)
+				logger.Info(printPrefix, "Exiting")
 				return
 			}
-			println(printPrefix, "Received event:", prettyPrint(res))
+			// Flush log queue in here after waking up
+			flushLogQueue(false)
 			// Exit if we receive a SHUTDOWN event
 			if res.EventType == extension.Shutdown {
-				println(printPrefix, "Received SHUTDOWN event")
-				println(printPrefix, "Exiting")
+				logger.Info(printPrefix, "Received SHUTDOWN event")
+				flushLogQueue(true)
+				logger.Info(printPrefix, "Exiting")
 				return
 			}
 		}
 	}
 }
+
 
 func prettyPrint(v interface{}) string {
 	data, err := json.MarshalIndent(v, "", "\t")
